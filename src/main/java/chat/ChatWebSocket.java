380504921c558ba2fac795b4622a10b81dfe2faa
= <=>
2018-03-19 15:57:39 +0300

    stepik 4.1 first commit

---
+++ src/main/java/chat/ChatWebSocket.java
@@ -0,0 +1,45 @@
+package chat;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+/**
+ * @author v.chibrikov
+ *         <p/>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p/>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+@SuppressWarnings("UnusedDeclaration")
+@WebSocket
+public class ChatWebSocket {
+
+    private Session session;
+
+
+    @OnWebSocketConnect
+    public void onOpen(Session session) {
+        this.session = session;
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(String data) {
+        sendString(data);
+    }
+
+    @OnWebSocketClose
+    public void onClose(int statusCode, String reason) {
+
+    }
+
+    public void sendString(String data) {
+        try {
+            session.getRemote().sendString(data);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
